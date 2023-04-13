f1ce81ccd91e09394a613f4e67565e7e143c2c89
Toan CSE <toan.nguyenminh@hcmut.edu.vn>
2023-04-14 00:43:24 +0700

    update Makefile and Font Italic

--- examCodeInWeb/editor/Editor.java
+++ examCodeInWeb/editor/Editor.java
@@ -6,6 +6,7 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.Font;
 
 public class Editor {
     public JTextArea textField;
@@ -26,6 +27,7 @@ public class Editor {
         JButton ctrlX = new JButton("Ctrl+X");
         JButton ctrlV = new JButton("Ctrl+V");
         JButton ctrlZ = new JButton("Ctrl+Z");
+        JButton  italic = new JButton("Italic");
         Editor editor = this;
         ctrlC.addActionListener(new ActionListener() {
             @Override
@@ -45,17 +47,28 @@ public class Editor {
                 executeCommand(new PasteCommand(editor));
             }
         });
+        italic.addActionListener(new ActionListener(){
+            @Override
+            public void actionPerformed(ActionEvent e){
+                executeCommand(new Italic(editor));
+            }
+        });
         ctrlZ.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 undo();
             }
         });
+        
         buttons.add(ctrlC);
         buttons.add(ctrlX);
         buttons.add(ctrlV);
         buttons.add(ctrlZ);
+        buttons.add(italic);
         content.add(buttons);
+
+        Font font = new Font(Font.SERIF, Font.PLAIN, 19);
+        textField.setFont(font);
         frame.setSize(450, 200);
         frame.setLocationRelativeTo(null);
         frame.setVisible(true);
